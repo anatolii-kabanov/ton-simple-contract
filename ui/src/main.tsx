368c9c7f103b50ed6b60a4b9f2cba91ad007a7de
Anatolii Kabanov <4akabanov@gmail.com>
2024-10-01 13:34:00 -0400

    - Ton connect - Manifest

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+
+const manifestUrl = '';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <TonConnectUIProvider manifestUrl={manifestUrl}>
+      <App />
+    </TonConnectUIProvider>
   </StrictMode>,
-)
+);
+
