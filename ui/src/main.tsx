2b3e9c986b36db78778fca3d9c3c9c0a8eb35d3b
Anatolii Kabanov <4akabanov@gmail.com>
2024-10-02 11:27:39 -0400

    - File names update

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import App from './appp';
+import App from './app';
 
 const manifestUrl = 'https://anatolii-kabanov.github.io/ton-simple-contract/ui/tonconnect-manifest.json';
 
