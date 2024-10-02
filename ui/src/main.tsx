59c9fbc7b82203e9c138f4f91f595c9f83fb4639
Anatolii Kabanov <4akabanov@gmail.com>
2024-10-02 12:11:58 -0400

    - Updated link

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import { TonConnectUIProvider } from '@tonconnect/ui-react';
 import App from './app';
 
-const manifestUrl = 'https://anatolii-kabanov.github.io/ton-simple-contract/ui/tonconnect-manifest.json';
+const manifestUrl = 'https://anatolii-kabanov.github.io/ton-simple-contract/tonconnect-manifest.json';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
